945866a1fe700462cb1ef19193894759feff1fea
yimdayimda <yimdayimda@gmail.com>
2017-08-08 16:25:39 +0900

    cal 2 init

---
+++ Calculator2.java
@@ -0,0 +1,18 @@
+package com.mycompany.test;
+
+public class Calculator {
+
+  public static void main(String[] args){
+    int a = 4;
+    int b = 2;
+    System.out.println( sum(a,b));
+  }
+
+  private static int sum(int a, int b){
+    return a + b;
+  }
+
+  private static int sum(int a, int b){
+    return a - b;
+  }
+}
